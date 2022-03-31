ede8708797e8afc8ef7d29c3152a6160f5e1b3bf
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2022-03-31 19:35:28 -0300

    User stats page using Victory library

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,16 +1,36 @@
 import React from "react";
+import { STATS_GET } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import Error from "../Helper/Error";
 import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-   return (
-      <div>
-         <Head
-            title="Estatísticas"
-            description="Estatísticas do usuário do site Dogs"
-         />
-         Estatísticas
-      </div>
-   );
+   const { loading, error, data, request } = useFetch();
+
+   React.useEffect(() => {
+      async function getData() {
+         const { url, options } = STATS_GET();
+         await request(url, options);
+      }
+
+      getData();
+   }, [request]);
+
+   if (loading) return <Loading />;
+   if (error) return <Error error={error} />;
+   if (data) {
+      return (
+         <div>
+            <Head
+               title="Estatísticas"
+               description="Estatísticas do usuário do site Dogs"
+            />
+            <UserStatsGraphs data={data} />
+         </div>
+      );
+   } else return null;
 };
 
 export default UserStats;
